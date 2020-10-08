3107f7274c36d6f7fc9ef991ff6c7c388ebd7c6a
chasepoirier <chase@chasepoirier.com>
2020-10-08 14:15:20 -0400

    Testing subgraph

---
+++ src/helpers.ts
@@ -0,0 +1,152 @@
+/* eslint-disable prefer-const */
+import { BigInt, BigDecimal, Address } from "@graphprotocol/graph-ts";
+import { ERC20 } from "../generated/Block/ERC20";
+import { ERC20SymbolBytes } from "../generated/Block/ERC20SymbolBytes";
+import { ERC20NameBytes } from "../generated/Block/ERC20NameBytes";
+
+import { Factory as FactoryContract } from "../generated/templates/Token/Factory";
+import { Wallet } from "../generated/schema";
+
+export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
+export const FACTORY_ADDRESS = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
+
+export let ZERO_BI = BigInt.fromI32(0);
+export let ONE_BI = BigInt.fromI32(1);
+export let ZERO_BD = BigDecimal.fromString("0");
+export let ONE_BD = BigDecimal.fromString("1");
+export let BI_18 = BigInt.fromI32(18);
+
+export let factoryContract = FactoryContract.bind(
+  Address.fromString(FACTORY_ADDRESS)
+);
+
+export function exponentToBigDecimal(decimals: BigInt): BigDecimal {
+  let bd = BigDecimal.fromString("1");
+  for (let i = ZERO_BI; i.lt(decimals as BigInt); i = i.plus(ONE_BI)) {
+    bd = bd.times(BigDecimal.fromString("10"));
+  }
+  return bd;
+}
+
+export function bigDecimalExp18(): BigDecimal {
+  return BigDecimal.fromString("1000000000000000000");
+}
+
+export function convertEthToDecimal(eth: BigInt): BigDecimal {
+  return eth.toBigDecimal().div(exponentToBigDecimal(18 as any));
+}
+
+export function convertTokenToDecimal(
+  tokenAmount: BigInt,
+  exchangeDecimals: BigInt
+): BigDecimal {
+  if (exchangeDecimals == ZERO_BI) {
+    return tokenAmount.toBigDecimal();
+  }
+  return tokenAmount.toBigDecimal().div(exponentToBigDecimal(exchangeDecimals));
+}
+
+export function equalToZero(value: BigDecimal): boolean {
+  const formattedVal = parseFloat(value.toString());
+  const zero = parseFloat(ZERO_BD.toString());
+  if (zero == formattedVal) {
+    return true;
+  }
+  return false;
+}
+
+export function isNullEthValue(value: string): boolean {
+  return (
+    value ==
+    "0x0000000000000000000000000000000000000000000000000000000000000001"
+  );
+}
+
+export function fetchTokenSymbol(tokenAddress: Address): string {
+  // hard coded override
+  if (
+    tokenAddress.toHexString() == "0xe0b7927c4af23765cb51314a0e0521a9645f0e2a"
+  ) {
+    return "DGD";
+  }
+
+  let contract = ERC20.bind(tokenAddress);
+  let contractSymbolBytes = ERC20SymbolBytes.bind(tokenAddress);
+
+  // try types string and bytes32 for symbol
+  let symbolValue = "unknown";
+  let symbolResult = contract.try_symbol();
+  if (symbolResult.reverted) {
+    let symbolResultBytes = contractSymbolBytes.try_symbol();
+    if (!symbolResultBytes.reverted) {
+      // for broken pairs that have no symbol function exposed
+      if (!isNullEthValue(symbolResultBytes.value.toHexString())) {
+        symbolValue = symbolResultBytes.value.toString();
+      }
+    }
+  } else {
+    symbolValue = symbolResult.value;
+  }
+
+  return symbolValue;
+}
+
+export function fetchTokenName(tokenAddress: Address): string {
+  // hard coded override
+  if (
+    tokenAddress.toHexString() == "0xe0b7927c4af23765cb51314a0e0521a9645f0e2a"
+  ) {
+    return "DGD";
+  }
+
+  let contract = ERC20.bind(tokenAddress);
+  let contractNameBytes = ERC20NameBytes.bind(tokenAddress);
+
+  // try types string and bytes32 for name
+  let nameValue = "unknown";
+  let nameResult = contract.try_name();
+  if (nameResult.reverted) {
+    let nameResultBytes = contractNameBytes.try_name();
+    if (!nameResultBytes.reverted) {
+      // for broken exchanges that have no name function exposed
+      if (!isNullEthValue(nameResultBytes.value.toHexString())) {
+        nameValue = nameResultBytes.value.toString();
+      }
+    }
+  } else {
+    nameValue = nameResult.value;
+  }
+
+  return nameValue;
+}
+
+export function fetchTokenDecimals(tokenAddress: Address): BigInt {
+  let contract = ERC20.bind(tokenAddress);
+  // try types uint8 for decimals
+  let decimalValue = null;
+  let decimalResult = contract.try_decimals();
+  if (!decimalResult.reverted) {
+    decimalValue = decimalResult.value;
+  }
+  return BigInt.fromI32(decimalValue);
+}
+
+export function createWallet(address: Address): void {
+  let wallet = Wallet.load(address.toHexString());
+  if (wallet === null) {
+    wallet = new Wallet(address.toHexString());
+    wallet.tokens = [];
+    wallet.save();
+  }
+}
+
+export function addToken(walletAddress: Address, tokenAddress: string): void {
+  let wallet = Wallet.load(walletAddress.toHexString());
+
+  const idx = wallet.tokens.indexOf(tokenAddress);
+
+  if (idx === -1) {
+    wallet.tokens.push(tokenAddress);
+    wallet.save();
+  }
+}
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,22 +1,94 @@
-import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
-import { Gravatar } from '../generated/schema'
+import { PairCreated } from "../generated/templates/Token/Factory";
+import { Token as TokenTemplate } from "../generated/templates";
+import { Token } from "../generated/schema";
+import {
+  DataSourceContext,
+  ethereum,
+  dataSource,
+  log,
+} from "@graphprotocol/graph-ts";
+import {
+  addToken,
+  createWallet,
+  fetchTokenDecimals,
+  fetchTokenName,
+  fetchTokenSymbol,
+} from "./helpers";
+import { Transfer } from "../generated/Block/ERC20";
 
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+// export function handleNewGravatar(event: NewGravatar): void {
+//   let gravatar = new Gravatar(event.params.id.toHex())
+//   gravatar.owner = event.params.owner
+//   gravatar.displayName = event.params.displayName
+//   gravatar.imageUrl = event.params.imageUrl
+//   gravatar.save()
+// }
+
+// export function handleUpdatedGravatar(event: UpdatedGravatar): void {
+//   let id = event.params.id.toHex()
+//   let gravatar = Gravatar.load(id)
+//   if (gravatar == null) {
+//     gravatar = new Gravatar(id)
+//   }
+//   gravatar.owner = event.params.owner
+//   gravatar.displayName = event.params.displayName
+//   gravatar.imageUrl = event.params.imageUrl
+//   gravatar.save()
+// }
+
+export function handleTransfer(event: Transfer): void {
+  let context = dataSource.context();
+  let contractAddress = context.getString("contractAddress");
+
+  createWallet(event.params.to);
+
+  addToken(event.params.to, contractAddress);
 }
 
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
+export function handleNewPair(event: PairCreated): void {
+  let token0 = Token.load(event.params.token0.toHexString());
+  let token1 = Token.load(event.params.token1.toHexString());
+
+  // fetch info if null
+  if (token0 == null) {
+    token0 = new Token(event.params.token0.toHexString());
+    token0.symbol = fetchTokenSymbol(event.params.token0);
+    token0.name = fetchTokenName(event.params.token0);
+
+    let decimals = fetchTokenDecimals(event.params.token0);
+    // bail if we couldn't figure out the decimals
+    if (decimals === null) {
+      log.debug("mybug the decimal on token 0 was null", []);
+      return;
+    }
+
+    token0.decimals = decimals;
+
+    let context = new DataSourceContext();
+    context.setBytes("contractAddress", event.params.token0);
+    TokenTemplate.createWithContext(event.params.token0, context);
   }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+
+  if (token1 == null) {
+    token1 = new Token(event.params.token1.toHexString());
+    token1.symbol = fetchTokenSymbol(event.params.token1);
+    token1.name = fetchTokenName(event.params.token1);
+
+    let decimals = fetchTokenDecimals(event.params.token1);
+    // bail if we couldn't figure out the decimals
+    if (decimals === null) {
+      log.debug("mybug the decimal on token 1 was null", []);
+      return;
+    }
+
+    token1.decimals = decimals;
+
+    let context = new DataSourceContext();
+    context.setBytes("contractAddress", event.params.token1);
+    TokenTemplate.createWithContext(event.params.token1, context);
+  }
+
+  // save updated values
+  token0.save();
+  token1.save();
 }
